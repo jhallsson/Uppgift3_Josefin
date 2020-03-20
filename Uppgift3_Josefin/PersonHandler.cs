cd3474d60997e9a86107527fc7a953f14c71e2a4
Josefin - elevdator <Josefin@DESKTOP-RRN9P3L>
2020-03-20 09:23:13 +0100

    metod CreatePerson() tillagd. Allt funkar

--- Uppgift3_Josefin/PersonHandler.cs
+++ Uppgift3_Josefin/PersonHandler.cs
@@ -8,19 +8,25 @@ namespace Uppgift3_Josefin
     {
         public Person CreatePerson(int age, string fName, string lName, double height, double weight)
         {
-            Person kalle = new Person();
-            return kalle;
+            Person person = new Person { 
+                Age = age, 
+                FirstName = fName, 
+                LastName = lName, 
+                Height = height, 
+                Weight = weight 
+            };
+            return person;
         }
         public void SetAge(Person pers, int age)
         {
             
-            //ToDo: Instansera ny person? 
-
             //ToDo: Sätt pers Age till age
             pers.Age = age;
             Console.WriteLine("Handler: "+pers.Age);
 
         }
-        
+        //ToDo: SetHeight
+        //ToDo: SetWeight
+        //ToDo: SetName / lastName
     }
 }
--- Uppgift3_Josefin/Program.cs
+++ Uppgift3_Josefin/Program.cs
@@ -6,40 +6,20 @@ namespace Uppgift3_Josefin
     {
         static void Main(string[] args)
         {
+            PersonHandler persHand = new PersonHandler();
+            
             //ToDo: Instansera ny person?
             Person pers= new Person();
-            int age = 25;
-
-            PersonHandler persHand= new PersonHandler();
             
-            //ToDo: Anropa SetAge(pers , age)
-            persHand.SetAge(pers ,age );
+            int age = 25;
+            persHand.SetAge(pers, age);
+            
 
-            Console.WriteLine(pers.Age);
+            Person kalle = persHand.CreatePerson(25, "Kalle","Karlsson",1.80,79.2);
+            
+            Console.WriteLine($"{kalle.FirstName} {kalle.LastName}");
+
         }
     }
 }
-/*  
-    
-vart anropa metoden SetAge      Main eller inuti Person?
- 
--Från main
--Du har precis innan skapat ett person objekt skicka med det och den nya åldern
-Sätt åldern på personobjektet
-titta före och efter att det är ändrat!
 
-C# är pass by value
-Men en referens typ håller bara adressen till det faktiska minnesplatsen för objektet
-Det är den du skickar
-Så ändras objektet i handlern
-Så pekar den gammla variabeln på samma objekt
-
-    
-     * 
-     
-     1 Main: Deklarera ny Person kalle. Instansera. Sätt Kalle.Age - skicka till Person propertyt
- *  2 Person-property(?): Kalla på personHandler.SetAge - skicka med Person-värde (pers) och int-värde (age)
- *  
- *  1 Main: Deklarera ny Person kalle. Instansera. Sätt Kalle.Age - skicka till PersonHandler.SetAge?
- *  2 PersonHandler: SetAge skapar Person pers och sätter age-attributet till skickat Age property
-     */
